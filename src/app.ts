448158b6838da66371a70e0d435293cbee7c5763
JoaoGabrielFigueira <joaogabrielfigueira89@gmail.com>
2025-05-25 12:51:10 -0300

    adding server

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,11 @@ import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://192.168.10.8:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler); // Middleware de erro
