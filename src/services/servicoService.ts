adbfaeef3901e241b52c2b738d7920fc5a73bfb7
PedroOliveiraWish <pedrowebdeveloper.oliveira@gmail.com>
2025-06-11 10:39:38 -0300

    FIX: corrigindo caminho

--- src/services/servicoService.ts
+++ src/services/servicoService.ts
@@ -1,5 +1,19 @@
 import { pool } from "../database/database";
-import { Servico } from "../types/servico";
+
+export interface Servico {
+    id: number;
+    custo: number;
+    preco: number;
+    cor_vidro: string;
+    largura: number;
+    altura: number;
+    fechadura: string;
+    cor_aluminio: string;
+    puxador: string;
+    estado: string;
+    sub_produto_id: number;
+    orcamento_id: number;
+}
 
 import { OrcamentoService } from "./OrcamentoService";
 
